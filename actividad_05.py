9948aa8cc6032d332822b629a53a073ceb73812b
Santiago <sjbatz@correo.url.edu.gt>
2025-07-22 09:41:50 -0600

    MENÚ CON CICLO WHILE Y MATCH CASE SIN FUNCIONALIDADES

---
+++ actividad_05.py
@@ -0,0 +1,40 @@
+while True:
+    print(f"MENÚ DE ANÁLISIS DE VENTAS")
+    print(f"1. Ingresar lista de ventas")
+    print(f"2. Mostrar todas las ventas realizadas")
+    print(f"3. Calcular venta más alta y baja")
+    print(f"4. Promedio de ventas")
+    print(f"5. Dias de buena venta")
+    print(f"6. Buscar ventas")
+    print(f"7. Clasificacion de ventas")
+    print(f"8. Salir")
+    option_user = input(f"Ingrese una opción: ")
+
+    match option_user:
+        case "1":
+            print(f"Ingresar lista de ventas")
+
+        case "2":
+            print(f"Mostrar todas las ventas realizadas")
+
+        case "3":
+            print(f"Venta más baja y alta")
+
+        case "4":
+            print(f"Promedio de ventas")
+
+        case "5":
+            print(f"Dias de buena venta")
+
+        case "6":
+            print(f"Buscar ventas")
+
+        case "7":
+            print(f"Clasificacion de ventas")
+
+        case "8":
+            print(f"Salio del programa...")
+            break
+
+        case _:
+            print(f"Ingreso mal un valor")
